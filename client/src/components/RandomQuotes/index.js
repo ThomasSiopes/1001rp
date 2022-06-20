08e0695f9ad025d0be74852e2f81d43fece5c06c
theeshyguy <t.siopes00@yahoo.com>
2022-06-20 17:28:57 -0400

    restructured quote bodies and minor error fixes

--- client/src/components/RandomQuotes/index.js
+++ client/src/components/RandomQuotes/index.js
@@ -4,7 +4,6 @@ import { useQuery } from "@apollo/client";
 
 import { Card, Button, Carousel } from "react-bootstrap";
 
-import TopicButton from "../TopicButton";
 import AuthorButton from "../AuthorButton";
 
 import { QUERY_QUOTE_ALL } from "../../utils/queries";
@@ -26,28 +25,6 @@ const RandomAuthors = () => {
     }
     
     return(
-        // <Carousel variant="dark" indicators={false} controls={true} nextIcon={<Button variant={"light"}><strong>{`>`}</strong></Button>} prevIcon={<Button variant={"light"}><strong>{`<`}</strong></Button>}>
-        //     {randomQuotes.map((index) => (
-        //         <Carousel.Item key={"Carousel" + index._id}>
-        //                 <Card>
-        //                     <Card.Header className="text-center">Random Quotes</Card.Header>
-        //                     <Link to={`/quote/${index.realID}`}>
-        //                         <Card.Body className="pb-0">
-        //                             <Card.Text className="text-black font-poppins">"{index.quoteText}"</Card.Text>
-        //                         </Card.Body>
-        //                     </Link>
-        //                     <Card.Body className="pb-4"><Card.Text><strong><AuthorButton type={"link"} name={index.author}/></strong></Card.Text></Card.Body>
-        //                     {index.topics.length !== 0 &&
-        //                         <Card.Footer className="text-center">
-        //                             {index.topics.map((index2) => (
-        //                                 <span key={index2} className="mx-1"><TopicButton type={"button"} name={index2}/></span>
-        //                             ))}
-        //                         </Card.Footer>
-        //                     }
-        //                 </Card>
-        //         </Carousel.Item>
-        //     ))}
-        // </Carousel>
         <Card>
             <Card.Header className="text-center">Random Quotes</Card.Header>
             <Card.Body className="px-0">
